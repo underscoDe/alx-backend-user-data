9bbc9ef22af23d9f2a5a074cb06549ee1002244d
Arsene Awounou <developer.awounou@gmail.com>
2022-07-21 22:54:55 +0100

    re.sub

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 """
 
 
-from re import sub
+import re
 from typing import List
 
 
@@ -11,6 +11,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ that returns the log message obfuscated"""
     for field in fields:
-        log_msg = sub(field + '=.*?' + separator,
-                      field + '=' + redaction + separator, message)
+        log_msg = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return log_msg
