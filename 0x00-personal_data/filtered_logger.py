60f99f54193d3ea888cacd82e33cd6e8cbe2da05
Arsene Awounou <developer.awounou@gmail.com>
2022-07-21 22:41:41 +0100

    reindent block causing error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,16 +7,11 @@ from re import sub
 from typing import List
 
 
-def filter_datum(
-    fields: List[str],
-    redaction: str,
-    message: str,
-    separator: str
-) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """ that returns the log message obfuscated
     """
     for field in fields:
         log_msg = sub(field + '=.*?' + separator,
-                      field + '=' + redaction + separator,
-                      message)
-        return log_msg
+                      field + '=' + redaction + separator, message)
+    return log_msg
