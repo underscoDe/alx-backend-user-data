315bf872efbf6dbc9a7c08dad801d51d656d52a3
Arsene Awounou <developer.awounou@gmail.com>
2022-07-28 22:42:07 +0100

    test 401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized/", strict_slashes=False)
+def unauthorized() -> str:
+    """Tests the 401 error handler"""
+    abort(401)
