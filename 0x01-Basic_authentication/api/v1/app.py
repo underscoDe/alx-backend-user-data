31e5db9803603b09f1130a0b596143d826591568
Arsene Awounou <developer.awounou@gmail.com>
2022-07-28 22:31:47 +0100

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,6 +25,11 @@ def unauthorized(error) -> str:
     """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Error handler for 403 forbidden"""
+    return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
