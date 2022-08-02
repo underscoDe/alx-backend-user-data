a83dacce3c72e50fcaf37a0e42d168d5d45d34e5
Arsene Awounou <developer.awounou@gmail.com>
2022-08-02 22:01:15 +0100

    mypy

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,11 +3,6 @@
 """
 
 import bcrypt
-from sqlalchemy.orm.exc import NoResultFound
-from db import DB
-from user import User
-import bcrypt
-from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
