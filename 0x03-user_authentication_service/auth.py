516d53a5466256c84c1506e873fc3119b35c6704
Arsene Awounou <developer.awounou@gmail.com>
2022-08-02 21:57:51 +0100

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+"""defining an authentication method"""
+
+import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
+import bcrypt
+from uuid import uuid4
+
+
+def _hash_password(password: str) -> str:
+    """takes in a password string
+    argument and returns a bytes"""
+
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
