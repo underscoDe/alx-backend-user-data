90729247a1a9e97ee348e31096d5f41f935f4c82
Arsene Awounou <developer.awounou@gmail.com>
2022-08-02 22:44:06 +0100

    Auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
 import bcrypt
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
@@ -14,6 +15,12 @@ def _hash_password(password: str) -> str:
     argument and returns a bytes"""
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Unique ID"""
+
+    return str(uuid4())
 
 
 class Auth:
@@ -31,3 +38,21 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """valid_login"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """create _session"""
+        try:
+            user = self._db.find_user_by(email=email)
+            sess_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=sess_id)
+            return sess_id
+        except NoResultFound:
+            return
