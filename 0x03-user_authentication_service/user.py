56364185970d756b869f1d693be990200d0a30b3
Arsene Awounou <developer.awounou@gmail.com>
2022-08-02 19:22:44 +0100

    pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,7 +13,6 @@ class User(BaseModel):
     """
     __tablename__ = 'users'
 
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
